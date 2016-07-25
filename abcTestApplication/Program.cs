3ba6c40acecac587c97570980cbb7662a29c66cd
Matthew McDaid Groves <matthew.groves@utexas.edu>
2016-07-25 16:06:48 -0500

    console.readline to close console.

--- abcTestApplication/Program.cs
+++ abcTestApplication/Program.cs
@@ -10,7 +10,9 @@ namespace abcTestApplication
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("here we go");
             Console.ReadLine();
+
         }
     }
 }
